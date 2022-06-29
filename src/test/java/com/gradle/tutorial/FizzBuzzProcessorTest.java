4d912f76b64b7cf18882daf508bce1683fd80520
jaseem <jaseeeem61@gmail.com>
2022-06-29 12:43:15 +0530

    tutorial for gradle done| FizzBuzz Problem

---
+++ src/test/java/com/gradle/tutorial/FizzBuzzProcessorTest.java
@@ -0,0 +1,35 @@
+package com.gradle.tutorial;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class FizzBuzzProcessorTest {
+    @Test
+    public void FizzBuzzNormalNumbers() {
+
+        FizzBuzzProcessor fb = new FizzBuzzProcessor();
+        Assertions.assertEquals("1", fb.convert(1));
+        Assertions.assertEquals("2", fb.convert(2));
+    }
+
+    @Test
+    public void FizzBuzzThreeNumbers() {
+
+        FizzBuzzProcessor fb = new FizzBuzzProcessor();
+        Assertions.assertEquals("Fizz", fb.convert(3));
+    }
+
+    @Test
+    public void FizzBuzzFiveNumbers() {
+
+        FizzBuzzProcessor fb = new FizzBuzzProcessor();
+        Assertions.assertEquals("Buzz", fb.convert(5));
+    }
+
+    @Test
+    public void FizzBuzzThreeAndFiveNumbers() {
+
+        FizzBuzzProcessor fb = new FizzBuzzProcessor();
+        Assertions.assertEquals("Buzz", fb.convert(5));
+    }
+}
